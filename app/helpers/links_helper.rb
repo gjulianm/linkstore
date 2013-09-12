36563f49d9589b6a9a568845e52c35a42f847f18
Guillermo Julián <gjulian93@gmail.com>
2013-09-12 20:30:42 +0200

    Don't push without testing.

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -2,32 +2,33 @@ require 'action_view'
 include ActionView::Helpers::DateHelper
 
 module LinksHelper
-	def self.to_relative_date date
+  def self.to_relative_date date
 
-		from_time = Time.now
+    from_time = Time.now
 
-		distance_of_time_in_words(from_time, date)
-	end
+    distance_of_time_in_words(from_time, date)
+  end
 
-	def self.remove_old
-		old_threshold = 1.day.ago
-		Link.where('updated_at < ? and done = ?', old_threshold, true ).destroy_allend
+  def self.remove_old
+    old_threshold = 1.day.ago
+    Link.where('updated_at < ? and done = ?', old_threshold, true ).destroy_allend
+  end
+  
+  def self.search_done_links feed
+    begin
+      links = Link.all
+      res = RestClient.get feed
 
-	def self.search_done_links feed
-		begin
-			links = Link.all
-			res = RestClient.get feed
-
-			if res.code == 200
-				body = res.body
-				links.each do |l|
-					if body.index l.url
-						l.done = true
-						l.save
-					end
-				end
-			end
-		rescue
-		end
-	end
+      if res.code == 200
+        body = res.body
+        links.each do |l|
+          if body.index l.url
+            l.done = true
+            l.save
+          end
+        end
+      end
+    rescue
+    end
+  end
 end
