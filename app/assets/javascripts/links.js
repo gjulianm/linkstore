531c309b73591d7b5d9785803a0f0b54d1381861
Guillermo Julián <gjulian93@gmail.com>
2013-11-23 18:35:47 +0100

    Improve priority color change

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -1,26 +1,26 @@
 function grabLink(id) {
 	$('#editor' + id).text("...");
-	$.get('/link/set_editor/' + id, function () {
+	$.get('/link/set_editor/' + id, function() {
 		$('#editor' + id).text("Grabbed by me");
 	});
 }
 
-function releaseLink(id) {	
+function releaseLink(id) {
 	$('#editor' + id).text("...");
-	$.get('/link/release/' + id, function () {
+	$.get('/link/release/' + id, function() {
 		$('#editor' + id).text("Released!");
 	});
 }
 
 function markAsDone(id) {
-	$('#done'+id).text("...");
+	$('#done' + id).text("...");
 	$.get('/link/done/' + id, function() {
-		$('#done'+id).text("Done!");
+		$('#done' + id).text("Done!");
 	});
 }
 
 function remove(id) {
-	$('#remove'+id).text("...");
+	$('#remove' + id).text("...");
 	$.get('/link/remove/' + id, function() {
 		location.reload(); // Well...
 	});
@@ -35,8 +35,16 @@ function addComment(id) {
 }
 
 function setPriority(id, priority) {
-	$.post('/api/links/' + id + '/priority', { 'priority': priority });
-	$('#link-' + id).addClass("priority-" + priority);
+	$.post('/api/links/' + id + '/priority', {
+		'priority': priority
+	});
+	var item = $('#link-' + id);
+	for (var i = 0; i < 3; i++) {
+		if (i == priority)
+			item.addClass("priority-" + priority);
+		else
+			item.removeClass("priority-" + i);
+	}
 	$('#priorityOptions' + id).hide();
 	$('#priorityLink' + id).show();
 }
